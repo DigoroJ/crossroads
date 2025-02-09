41c4a88411b5105b2e5812e2ad8f1c06c6d266eb
unknown <digorojoseph@gmail.com>
2025-02-09 13:47:03 +0200

    update time

--- time.js
+++ time.js
@@ -1,1 +1,177 @@
-function _0x28f4(_0x131a70,_0xe2a01b){var _0xfba55f=_0xfba5();return _0x28f4=function(_0x28f471,_0x2cf0e4){_0x28f471=_0x28f471-0x6f;var _0x5f28c2=_0xfba55f[_0x28f471];return _0x5f28c2;},_0x28f4(_0x131a70,_0xe2a01b);}var _0x46727b=_0x28f4;(function(_0x18e279,_0x204145){var _0x1c1377=_0x28f4,_0x49b788=_0x18e279();while(!![]){try{var _0x255ced=-parseInt(_0x1c1377(0x83))/0x1+parseInt(_0x1c1377(0x76))/0x2+parseInt(_0x1c1377(0xae))/0x3*(parseInt(_0x1c1377(0x98))/0x4)+-parseInt(_0x1c1377(0xa2))/0x5+-parseInt(_0x1c1377(0x92))/0x6*(-parseInt(_0x1c1377(0xa8))/0x7)+-parseInt(_0x1c1377(0xa7))/0x8+-parseInt(_0x1c1377(0x77))/0x9;if(_0x255ced===_0x204145)break;else _0x49b788['push'](_0x49b788['shift']());}catch(_0x45f580){_0x49b788['push'](_0x49b788['shift']());}}}(_0xfba5,0xc906f),document[_0x46727b(0x82)](_0x46727b(0xb0),()=>{var _0x278288=_0x46727b;const _0x3534c4=new Date();let _0x4261d8=[{'name':'Vision\x20Casting','department':_0x278288(0x8b),'date':new Date(_0x278288(0x84)),'time':_0x278288(0x97)},{'name':_0x278288(0x8d),'department':_0x278288(0xad),'date':new Date(_0x278288(0x87)),'time':'09:00'},{'name':'Brothers\x20breakfast','department':_0x278288(0x71),'date':new Date(_0x278288(0x7c)),'time':''},{'name':_0x278288(0x8c),'department':_0x278288(0x72),'date':new Date(_0x278288(0x85)),'time':''},{'name':_0x278288(0x9e),'department':_0x278288(0x8b),'date':new Date('04-18-2025'),'time':_0x278288(0x89)},{'name':_0x278288(0xa1),'department':_0x278288(0x8b),'date':new Date(_0x278288(0x96)),'time':'09:00'},{'name':_0x278288(0xa6),'department':_0x278288(0x7e),'date':new Date('04-26-2025\x2009:00'),'time':_0x278288(0x89)},{'name':_0x278288(0x88),'department':_0x278288(0x81),'date':new Date('05-04-2025'),'time':''},{'name':'Pamphet\x20Drive\x20&\x20Open\x20Air','department':_0x278288(0x72),'date':new Date(_0x278288(0x6f)),'time':''},{'name':_0x278288(0x9b),'department':_0x278288(0x7a),'date':new Date(_0x278288(0xa9)),'time':''},{'name':_0x278288(0xb1),'department':_0x278288(0x90),'date':new Date(_0x278288(0xaa)),'time':_0x278288(0x80)},{'name':'All\x20day\x20@\x20Church','department':'Intercessors','date':new Date(_0x278288(0xa4)),'time':_0x278288(0x89)},{'name':_0x278288(0x78),'department':_0x278288(0x90),'date':new Date(_0x278288(0x94)),'time':_0x278288(0xab)},{'name':_0x278288(0x91),'department':_0x278288(0x72),'date':new Date('09-12-2025'),'time':''},{'name':_0x278288(0xac),'department':_0x278288(0x99),'date':new Date(_0x278288(0x9f)),'time':'09:00'},{'name':_0x278288(0x86),'department':'Church','date':new Date('09-24-2025'),'time':''},{'name':_0x278288(0x7f),'department':'Womens\x20Ministry','date':new Date(_0x278288(0x75)),'time':''},{'name':_0x278288(0x70),'department':_0x278288(0x8b),'date':new Date('11-08-2025'),'time':''},{'name':'Carols\x20By\x20Candle\x20Light','department':'Church','date':new Date(_0x278288(0x8a)),'time':''}];var _0x20ddf4={},_0x295138=[];for(const _0x2e6374 of _0x4261d8){_0x2e6374[_0x278288(0x7d)]>_0x3534c4&&_0x295138['push'](_0x2e6374);}_0x20ddf4=_0x295138[0x0],console[_0x278288(0xa0)](_0x278288(0x79),_0x4261d8);var _0x5dbd1b=0x3c-_0x3534c4[_0x278288(0xb2)]()+_0x20ddf4[_0x278288(0x7d)][_0x278288(0xb2)](),_0x274fef=_0x20ddf4[_0x278288(0x7d)]-_0x3534c4;_0x274fef=Math[_0x278288(0xaf)](_0x274fef/0xea60),newHr=Math[_0x278288(0x9c)](_0x274fef/0x3c),diffDays=Math[_0x278288(0x9c)](newHr/0x18),hours=newHr%0x18-0x1;(_0x1109c9=>0x3c)&&(_0x5dbd1b=_0x5dbd1b%0x3c,hours+=0x1);(_0x21aafb=>0x18)&&(hours=hours%0x18,diffDays+=0x1);document['getElementById'](_0x278288(0x9d))[_0x278288(0xa3)]=diffDays,document[_0x278288(0x7b)](_0x278288(0x8f))['innerText']=hours,document['getElementById']('mins')[_0x278288(0xa3)]=_0x5dbd1b;var _0x53b8ae=_0x20ddf4[_0x278288(0x93)],_0x26d7f9=String(_0x20ddf4[_0x278288(0x7d)])[_0x278288(0x95)](0x0,0xf);document[_0x278288(0x7b)](_0x278288(0xa5))[_0x278288(0xa3)]=_0x53b8ae,document[_0x278288(0x7b)](_0x278288(0x9a))[_0x278288(0xa3)]=_0x26d7f9,document[_0x278288(0x7b)](_0x278288(0x74))['innerText']=_0x53b8ae,document[_0x278288(0x7b)]('eventDate1')[_0x278288(0xa3)]=_0x26d7f9,document[_0x278288(0x7b)](_0x278288(0x73))[_0x278288(0xa3)]=_0x53b8ae,document[_0x278288(0x7b)](_0x278288(0x8e))[_0x278288(0xa3)]=_0x26d7f9;}));function _0xfba5(){var _0xf8d7c5=['15:00','4ajmSbX','Intercerssors','eventDate','Coffee\x20Bar','floor','days','Good\x20Friday\x20Celebration','09-20-2025\x2009:00','log','Easter\x20Sunday','1229295SIXNRs','innerText','07-26-2025\x2009:00','eventName','Marriage\x20Counselling\x20Seminar','8980440APcxUL','78827WhpCgu','07-05-2025','07-12-2025\x2022:00','18:00','Prayer\x20day\x20-\x20Come\x20pray\x20for\x20your\x20family','Worship\x20team','4537077JzUyFW','ceil','DOMContentLoaded','Stay\x20awake','getMinutes','06-07-2025','Banquet','Mens\x20Fellowship','Evangelism','eventName2','eventName1','10-18-2025','2098628fhCdLD','14834052SXxeXd','Concert','Events','Youth\x20Fellowship','getElementById','04-05-2025','date','Intercessors','Breast\x20Cancer\x20Awareness','22:00','\x20Womens\x20ministry','addEventListener','354469HsIjeR','02-09-2025\x2015:00','04-06-2025','Heritage\x20day\x20Celebration','03-22-2025\x2009:00','Roosterbrood\x20&\x20Braai\x20vleis','09:00','12-14-2025','Church','Recovery\x20And\x20Restoration\x20Service','Worship\x20seminar','eventDate2','hours','Worship\x20Team','Combined\x20Service\x20with\x20Youth','870yilMsU','name','09-06-2025\x2018:00','substring','04-20-2025\x2009:00'];_0xfba5=function(){return _0xf8d7c5;};return _0xfba5();}
+document.addEventListener('DOMContentLoaded', () => {
+    //const { format } = require('date-fns');
+    const now = new Date();
+
+    let events = [
+        {
+            "name": "Vision Casting",
+            "department": "Church",
+            "date": new Date('02-09-2025 15:00'),
+            "time": "15:00"
+        },
+        {
+            "name": "Worship seminar",
+            "department": "Worship team",
+            "date": new Date('03-22-2025 09:00'),
+            "time": "09:00"
+        },
+        {
+            "name": "Brothers breakfast",
+            "department": "Mens Fellowship",
+            "date": new Date('04-05-2025'),
+            "time": ""
+        },
+        {
+            "name": "Recovery And Restoration Service",
+            "department": "Evangelism",
+            "date": new Date('04-06-2025'),
+            "time": ""
+        },
+        {
+            "name": "Good Friday Celebration",
+            "department": "Church",
+            "date": new Date('04-18-2025'),
+            "time": "09:00"
+        },
+        {
+            "name": "Easter Sunday",
+            "department": "Church",
+            "date": new Date('04-20-2025 09:00'),
+            "time": "09:00"
+        },
+        {
+            "name": "Marriage Counselling Seminar",
+            "department": "Intercessors",
+            "date": new Date('04-26-2025 09:00'),
+            "time": "09:00"
+        },
+        {
+            "name": "Roosterbrood & Braai vleis",
+            "department": " Womens ministry",
+            "date": new Date('05-04-2025'),
+            "time": ""
+        },
+        {
+            "name": "Pamphet Drive & Open Air",
+            "department": "Evangelism",
+            "date": new Date('06-07-2025'),
+            "time": ""
+        },
+        {
+            "name": "Coffee Bar",
+            "department": "Youth Fellowship",
+            "date": new Date('07-05-2025'),
+            "time": ""
+        },
+        {
+            "name": "Stay awake",
+            "department": "Worship Team",
+            "date": new Date('07-12-2025 22:00'),
+            "time": "22:00"
+        },
+        {
+            "name": "All day @ Church",
+            "department": "Intercessors",
+            "date": new Date('07-26-2025 09:00'),
+            "time": "09:00"
+        },
+        {
+            "name": "Concert",
+            "department": "Worship Team",
+            "date": new Date('09-06-2025 18:00'),
+            "time": "18:00"
+        },
+        {
+            "name": "Combined Service with Youth",
+            "department": "Evangelism",
+            "date": new Date('09-12-2025'),
+            "time": ""
+        },
+        {
+            "name": "Prayer day - Come pray for your family",
+            "department": "Intercerssors",
+            "date": new Date('09-20-2025 09:00'),
+            "time": "09:00"
+        },
+        {
+            "name": "Heritage day Celebration",
+            "department": "Church",
+            "date": new Date('09-24-2025'),
+            "time": ""
+        },
+        {
+            "name": "Breast Cancer Awareness",
+            "department": "Womens Ministry",
+            "date": new Date('10-18-2025'),
+            "time": ""
+        },
+        {
+            "name": "Banquet",
+            "department": "Church",
+            "date": new Date('11-08-2025'),
+            "time": ""
+        },
+        {
+            "name": "Carols By Candle Light",
+            "department": "Church",
+            "date": new Date('12-14-2025'),
+            "time": ""
+        }
+        // ,
+        // {
+        //     "name": "",
+        //     "department": "",
+        //     "date": new Date(),
+        //     "time": ""
+        // },
+        // {
+        //     "name": "",
+        //     "department": "",
+        //     "date": new Date(),
+        //     "time": ""
+        // }
+    ]
+    var nextEvent = {}
+    var nextEvents = []
+    for (const event of events) {
+        if (event.date > now) {
+            nextEvents.push(event)  
+        }
+    }
+    nextEvent = nextEvents[0];
+    console.log('Events', events );
+
+    var minutes = 60 - now.getMinutes() + nextEvent.date.getMinutes()
+    var min = nextEvent.date - now
+    min = Math.ceil(min / 60000) 
+    newHr = Math.floor(min/60)
+    diffDays = Math.floor(newHr / 24)
+    hours = (newHr % 24 -1 )
+    if (minutes => 60) {
+        minutes = minutes % 60
+        hours += 1
+    }
+    if (hours => 24) {
+        hours = hours % 24
+       // diffDays += 1
+    }
+
+    //Count down
+    document.getElementById('days').innerText = diffDays;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('mins').innerText = minutes;
+
+    var eventName = nextEvent.name
+    var eventDate = String(nextEvent.date).substring(0,21)
+
+    //Next event
+    document.getElementById('eventName').innerText = eventName;
+    document.getElementById('eventDate').innerText = eventDate;
+
+    document.getElementById('eventName1').innerText = eventName;
+    document.getElementById('eventDate1').innerText = eventDate;
+
+    document.getElementById('eventName2').innerText = eventName;
+    document.getElementById('eventDate2').innerText = eventDate;
+    
+  });
